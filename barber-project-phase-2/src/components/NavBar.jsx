4566293ad17bfd7877861b9ba40a4ddf8bf75505
Kenneth <luo.kenneth1999@gmail.com>
2023-12-07 13:15:20 -0500

    Added a route to Slice icon that redirects back to home page

--- barber-project-phase-2/src/components/NavBar.jsx
+++ barber-project-phase-2/src/components/NavBar.jsx
@@ -1,16 +1,21 @@
 import { NavLink } from "react-router-dom";
 import React from "react";
 import "./NavBar.css";
-
+import { useNavigate } from "react-router-dom";
 
 
 /* define the NavBar component */
 function NavBar({isSearch, search, setSearch}) {
 
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate(`/`);
+  }
+
   return (
     <nav className="navbar">
       <div className="left-image">
-        <img src="https://media.discordapp.net/attachments/1169014161238401109/1181996408442736842/c16be0df4d505d93821edca14a589842.png?ex=658316f0&is=6570a1f0&hm=66f5a3aa68dc232f7e9c51159aee79623e07a87c27f10430b845b5213328b248&=&format=webp&quality=lossless&width=1782&height=936" alt="Logo"/>
+        <img onClick={handleClick} src="https://media.discordapp.net/attachments/1169014161238401109/1181996408442736842/c16be0df4d505d93821edca14a589842.png?ex=658316f0&is=6570a1f0&hm=66f5a3aa68dc232f7e9c51159aee79623e07a87c27f10430b845b5213328b248&=&format=webp&quality=lossless&width=1782&height=936" alt="Logo"/>
       </div>
       <div className="center-image">
       {!isSearch ? null :
@@ -21,7 +26,7 @@ function NavBar({isSearch, search, setSearch}) {
               </form>
           </div>}
       </div>
-      <div className="right-images">
+      <div class="right-images">
         <NavLink to="/home"><img className="nav-image" src="https://cdn.discordapp.com/attachments/1169014161238401109/1182077082168213604/Image_12-6-23_at_4.50_PM.jpg?ex=65836212&is=6570ed12&hm=6b8df38ed906e1a4cdd97c31f84bd18fe7ec04009f88c0d3092e3717cd527b2f&" alt="Home"/></NavLink>
         <NavLink to="/profile" className="nav-link"><img className="nav-image" src="https://cdn-icons-png.flaticon.com/512/3106/3106773.png" alt="Profile"/></NavLink>
       </div>
